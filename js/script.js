a4cd0576ae6418e94fc53bf3081f120a43f0b079
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-09-02 16:36:09 +0200

    base2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-console.log('Vue', Vue);
+console.log("Vue", Vue);
 
 Vue.config.devtools = true;
 
 const app = new Vue({
-  el: '#app',
-  data: {},
+  el: "#app",
+  data: {
+    musics: [],
+  },
   methods: {},
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        const response = res.data.response;
+        this.musics = response;
+      });
+  },
 });
