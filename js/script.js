38e4bbde112703d6ad4d93a863cccbd25e6d3579
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-09-02 21:16:45 +0200

    fine bonus

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const app = new Vue({
   el: "#app",
   data: {
     musics: [],
+    generi: [],
+    genere_selezionato: "",
   },
   methods: {},
   created() {
@@ -14,6 +16,16 @@ const app = new Vue({
       .then((res) => {
         const response = res.data.response;
         this.musics = response;
+
+        // girando nel'array music con forEach - se l'array generi non include music.genre allora puscia music.genre in generi
+        this.musics.forEach((music) => {
+          if (!this.generi.includes(music.genre)) {
+            this.generi.push(music.genre);
+          }
+        });
+        this.musics.sort(function (music1, music2) {
+          return parseInt(music2.year) - parseInt(music1.year);
+        });
       });
   },
 });
